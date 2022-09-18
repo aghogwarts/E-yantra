91a37c65038112b23aec89958f86da300b1eb5ee
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-09-19 00:56:52 +0530

    Fix T+1 issue and make it T

--- PAL_PY.py
+++ PAL_PY.py
@@ -2,7 +2,7 @@
 
 T = int(input(""))
 if T in range(1,26):
-    for i in range(0,(T+1)):
+    for i in range(0,(T)):
         string=(input("")).lower()
         if len(string) in range(2,101):
             if(string==string[::-1]):
