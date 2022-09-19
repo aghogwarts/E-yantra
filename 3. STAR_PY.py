a1be2984fc7fd3c6c1c934e2878bf4a21938b1e8
Rohan <79784578+rohan-k02@users.noreply.github.com>
2022-09-19 17:51:02 +0530

    Update and rename STAR_PY.py to 3. STAR_PY.py

---
+++ 3. STAR_PY.py
@@ -0,0 +1,15 @@
+# cook your dish here
+T = int(input(""))
+
+if T in range(1, 26):
+    for x in range(0, T):
+        N = int(input(""))
+        if N in range(1,101):
+            for i in range(N, 0, -1):
+                for j in range(1, i+1):
+                    if j % 5 == 0:
+                        print("#", end = "")
+                    else:
+                        print("*", end = "")
+                print("\r")
+                
