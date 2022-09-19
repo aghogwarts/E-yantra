c6d453fead1d9e28add7385db868d2b8b04bf934
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-09-19 18:02:26 +0530

    Update 1. PAL_PY.py

--- 1. PAL_PY.py
+++ 1. PAL_PY.py
@@ -1,10 +1,10 @@
 # cook your dish here
 
-T = int(input(""))
+T = int(input())
 
 if T in range(1,26):
     for i in range(0,(T)):
-        string=(input("")).lower()
+        string=(input()).lower()
         if len(string) in range(2,101):
             if(string==string[::-1]):
                 print("It is a palindrome")
