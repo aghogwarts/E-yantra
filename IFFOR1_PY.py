6a306a6f39b21a8b76e3d2ade262b4e0126713db
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-09-19 17:24:06 +0530

    Upload IFFOR1_PY.py
    Successful code for Task 2 in CodeChef

---
+++ IFFOR1_PY.py
@@ -0,0 +1,18 @@
+# cook your dish here
+
+T = int(input(""))
+
+if T in range(1,26):
+    for x in range(0,(T)):
+        n = int(input(""))
+        if n in range(0,101):
+            for i in range(0,n):
+                if i == 0:
+                    i = i + 3
+                    print(i, end=" ")
+                else:
+                    if i % 2 == 1:
+                        print(i*i, end=" ")
+                    else:
+                        print(i*2, end=" ")
+        print("\r")
