edd1805e1c26a7dd991489bad05ab5b5d313d5fe
Rohan <79784578+rohan-k02@users.noreply.github.com>
2022-09-21 21:27:05 +0530

    Update 2. IFFOR1_PY.py

--- 2. IFFOR1_PY.py
+++ 2. IFFOR1_PY.py
@@ -1,18 +1,20 @@
 # cook your dish here
-
-T = int(input())
+T = int(input(""))
 
 if T in range(1,26):
-    for x in range(0,(T)):
-        n = int(input())
+    for a in range(0,T):
+        n = int(input(""))
         if n in range(0,101):
             for i in range(0,n):
                 if i == 0:
-                    i = i + 3
-                    print(i, end=" ")
+                    print(i + 3, end = " ")
+                elif (i == n - 1):
+                    if ((i % 2) == 1):
+                        print(i*i)
+                    else:
+                        print(i*2)
                 else:
-                    if i % 2 == 1:
+                    if ((i % 2) == 1):
                         print(i*i, end=" ")
                     else:
-                        print(i*2, end=" ")
-        print("\r")
+                        print(i*2,end = " ")
