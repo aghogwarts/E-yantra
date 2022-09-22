01ffdcc9d3f621c4c58a9f6fd71f4f3279b7a0bb
Rohan <79784578+rohan-k02@users.noreply.github.com>
2022-09-22 21:14:36 +0530

    Update 09. SCOR_PY.py

--- 09. SCOR_PY.py
+++ 09. SCOR_PY.py
@@ -1,10 +1,33 @@
-T=int(input(""))
-no_stud=int(input(""))
-dictionary={}
-if T in range(1,26) and no_stud in range(2,101):
-    for i in range(0,no_stud):
-        item=input("")
-        x,y=item.split()
-        dictionary.update({x:y})
-keymax=max(dictionary, key= lambda x:dictionary[x] )        
-print(keymax)
+T = int(input())
+
+
+#count = 0
+if T in range(1,26):
+    for i in range(0,T):
+        student_name = []
+        score = []
+        N = int(input())
+        if N in range(2,101):
+            constraint = True
+            for a in range(0,N):
+                item = input().split()
+                student_name.append(item[0])
+                item[1] = float(item[1])
+                if not (item[1] >= 0.0 and item[1] <= 100.0):
+                    constraint = False
+                score.append(item[1])
+                #print(constraint)
+            if constraint == True:
+                maxname = []
+                maxscore = max(score)
+                for i in range(0,len(score)):
+                    if score[i] == maxscore:
+                        maxname.append(student_name[i])
+                maxname.sort()
+                
+                for i in maxname:
+                    print(i)
+                        
+        #count=count+1
+        #print(count)
+            
