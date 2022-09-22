88a8a91804705c35c6cc382ed4c481cb2338b806
Rohan <79784578+rohan-k02@users.noreply.github.com>
2022-09-22 15:15:10 +0530

    Update 1. PAL_PY.py

--- 1. PAL_PY.py
+++ 1. PAL_PY.py
@@ -4,9 +4,9 @@ T = int(input())
 
 if T in range(1,26):
     for i in range(0,(T)):
-        string=(input()).lower()
+        string = (input()).lower()
         if len(string) in range(2,101):
-            if(string==string[::-1]):
+            if(string == string[::-1]):
                 print("It is a palindrome")
             else:
                 print("It is not a palindrome")
