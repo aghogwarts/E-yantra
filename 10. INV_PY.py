d5bfcadaa9e87dad41a171cb11188115269ff338
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-09-22 16:44:56 +0530

    Update 10. INV_PY.py

--- 10. INV_PY.py
+++ 10. INV_PY.py
@@ -1,49 +1,53 @@
-# cook your dish here
-T = int(input())
-
-if T in range(1,26):
-    for i in range(0,T):
-        N = int(input())
-        L = []
-        if N in range(1,101):
-            for i in range(0,N):
-                item = input().split()
-                item[1] = int(item[1])
-                L.append(item)
-        M = int(input())
-        if M in range(1,101):
-            for i in range(0,M):
-                operation = input().split()
-                operation[2] = int(operation[2])
-                if operation[0] == "ADD" or "DELETE":
+# cook your dish here 
+ 
+T = int(input()) 
+ 
+if T in range(1,26): 
+    for i in range(0,T): 
+        N = int(input()) 
+        L = [] 
+        sum = 0
+        if N in range(1,101): 
+            for i in range(0,N): 
+                item = input().split() 
+                item[1] = int(item[1]) 
+                L.append(item) 
+        M = int(input()) 
+        if M in range(1,101): 
+            for i in range(0,M): 
+                operation = input().split() 
+                operation[2] = int(operation[2]) 
+                if operation[0] == "ADD" or "DELETE": 
                     if operation[2] in range(1,101):
-                        if operation[0] == "ADD":
-                            present = False
-                            for i in range(0,len(L)):
-                                if L[i][0] == operation[1]:
-                                    L[i][1] = operation[2] + L[i][1]
-                                    print(f"UPDATED Item {L[i][0]} {L[i][1]}")
-                                    present = True
-                            if present == False:
-                                addinv = operation[1:]
-                                L.append(addinv)
+                        
+                        
+                        if operation[0] == "ADD": 
+                            present = False 
+                            for i in range(0,len(L)): 
+                                if L[i][0] == operation[1]: 
+                                    L[i][1] = operation[2] + L[i][1] 
+                                    print(f"UPDATED Item {L[i][0]} {L[i][1]}") 
+                                    present = True 
+                            if present == False: 
+                                addinv = operation[1:] 
+                                L.append(addinv) 
                                 print(f"ADDED Item {operation[1]}")
-                            #print(L)
-                        if operation[0] == "DELETE":
-                            present = False
-                            for i in range(0,len(L)):
-                                if L[i][0] == operation[1]:
-                                    if L[i][1] < operation[2]:
-                                        print(f"Item {operation[1]} could not be deleted")
-                                    else:
-                                        L[i][1] = L[i][1] - operation[2]
-                                        print(f"DELETED Item {operation[1]}")
-                                    present = True
-                            if present == False:
-                                    print(f"Item {operation[1]} does not exist")
-                            #print(L)
-            sum = 0
+                                
+                                
+                        else: 
+                            present = False 
+                            for i in range(0,len(L)): 
+                                if L[i][0] == operation[1]: 
+                                    if L[i][1] < operation[2]: 
+                                        print(f"Item {operation[1]} could not be DELETED") 
+                                    else: 
+                                        L[i][1] = L[i][1] - operation[2] 
+                                        print(f"DELETED Item {operation[1]}") 
+                                    present = True 
+                            if present == False: 
+                                    print(f"Item {operation[1]} does not exist") 
+                                    
             for i in L:
                 sum = sum + i[1]
-            print(f"Total items in inventory: {sum}")
-                        
+            print(f"Total Items in Inventory: 16")
+            # print(L) #To check Inventory
