58d8047dbbcd5f8a2667d48a4b98495e1ef9a571
Rohan <79784578+rohan-k02@users.noreply.github.com>
2022-11-03 21:33:46 +0530

    Update controller.py

--- task_1/controller.py
+++ task_1/controller.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 
 import rospy
 
@@ -32,23 +32,45 @@ def main():
 	# initialising node named "controller"
 	
 	# Initialze Publisher and Subscriber
-	 rospy.Subscriber("/odom", Odometry, odometryCb)
-	 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+	rospy.Subscriber("/odom", Odometry, odometryCb)
+	pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 	# We'll leave this for you to figure out the syntax for
 	# initialising publisher and subscriber of cmd_vel and odom respectively
 
 	# Declare a Twist message
 	vel = Twist()
+
 	# Initialise the required variables to 0
-	float x_d = 0
-	float y_d = 0
-	float theta_d = 0
-	int i = 0
-	float e_x_g = 0
-	float e_y_g = 0
-	float e_theta_gl = 0
-	float e_x_l = 0
-	float e_y_l = 0
+
+	#Tolerances
+
+	#desired pose
+	x_d = 0
+	y_d = 0
+	theta_d = 0
+	
+	# desired pose Array index position
+	i = 0
+
+	#Error in global frame
+	e_x_g = 0
+	e_y_g = 0
+	e_theta_gl = 0
+
+	# Error in local frame
+	e_x_l = 0
+	e_y_l = 0
+
+	#Variables for PID controller
+	vel_x = 0
+	vel_y = 0
+	vel_z = 0
+	
+	# Need to find these values by tuning and trial-error.
+	kp_x = 0
+	kp_y = 0
+	kp_theta = 0
+	
 	
 	# <This is explained below>
 	
@@ -59,12 +81,9 @@ def main():
 	# For ex: x_d, y_d, theta_d (in **meters** and **radians**) for defining desired goal-pose.
 	# and also Kp values for the P Controller
 
-	#
-	# 
-	# Control Loop goes here
-	#
-	#
-    while not rospy.is_shutdown():
+	#control loop
+	
+	while not rospy.is_shutdown():
 
 		# 
 		# Find error (in x, y and theta) in global frame
@@ -74,14 +93,19 @@ def main():
 		e_x_g = x_d - hola_x
 		e_y_g = y_d - hola_y
 		e_theta_gl = theta_d - hola_theta
+
+		if((abs(e_x_g) <= 0.05) and (abs(e_y_g) <= 0.05) and (abs(e_theta_gl) <= 0.0174533)):
+			break
+		
+
  
 		# (Calculate error in body frame)
 		# But for Controller outputs robot velocity in robot_body frame, 
 		# i.e. velocity are define is in x, y of the robot frame, 
 		# Notice: the direction of z axis says the same in global and body frame
 		# therefore the errors will have have to be calculated in body frame.
-		e_x_l = e_x_g*cos(hola_theta) - e_y_g*sin(hola_theta)
-		e_y_l = e_x_g*sin(hola_theta) + e_y_g*cos(hola_theta)
+		e_x_l = e_x_g*math.cos(hola_theta) - e_y_g*math.sin(hola_theta)
+		e_y_l = e_x_g*math.sin(hola_theta) + e_y_g*math.cos(hola_theta)
 
 
 		
@@ -89,7 +113,9 @@ def main():
 	
 		# Finally implement a P controller 
 		# to react to the error with velocities in x, y and theta.
-
+		vel_x = kp_x*e_x_l
+		vel_y = kp_y*e_y_l
+		vel_z = kp_theta*e_theta_gl
 
 
 		# Safety Check
@@ -100,9 +126,15 @@ def main():
 		vel.linear.x = vel_x
 		vel.linear.y = vel_y
 		vel.angular.z = vel_z
-
 		pub.publish(vel)
 		rate.sleep()
+		
+	vel.linear.x = 0
+	vel.linear.y = 0
+	vel.angular.z = 0
+	pub.publish(vel)
+	rate.sleep()
+		
 
 
 if __name__ == "__main__":
